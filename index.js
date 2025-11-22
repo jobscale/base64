d7ceea2dfe831379163f62cd8c7a5eac260fe4ca
jobscale <jobscale@users.noreply.github.com>
2025-11-23 00:08:07 +0900

    es module

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-(() => {
-  class Base64 {
-    constructor() {
-      this.base64 = this;
-      this.Base64 = Base64;
-    }
-
-    encode(str) {
-      if (typeof window !== 'undefined') {
-        return btoa(unescape(encodeURIComponent(str)));
-      }
-      return Buffer.from(str).toString('base64');
-    }
-
-    decode(str) {
-      if (typeof window !== 'undefined') {
-        return decodeURIComponent(String.escape(atob(str)));
-      }
-      return Buffer.from(str, 'base64').toString();
-    }
+export class Base64 {
+  constructor() {
+    this.base64 = this;
+    this.Base64 = Base64;
   }
 
-  const base64 = new Base64();
-  if (typeof module !== 'undefined') module.exports = base64;
-  if (typeof window !== 'undefined') window.base64 = base64;
-})();
+  encode(str) {
+    if (typeof window !== 'undefined') {
+      return btoa(unescape(encodeURIComponent(str)));
+    }
+    return Buffer.from(str).toString('base64');
+  }
+
+  decode(str) {
+    if (typeof window !== 'undefined') {
+      return decodeURIComponent(String.escape(atob(str)));
+    }
+    return Buffer.from(str, 'base64').toString();
+  }
+}
+
+export const base64 = new Base64();
+export default {
+  Base64,
+  base64,
+};
